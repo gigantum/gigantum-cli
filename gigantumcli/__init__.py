8a282ba81ce71817e7df15890870351b5421c0e7
Dean Kleissas <dean@gigantum.com>
2019-01-03 11:18:12 -0500

    Update version

--- gigantumcli/__init__.py
+++ gigantumcli/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 FlashX, LLC
+# Copyright (c) 2018 FlashX, LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,4 +19,4 @@
 # SOFTWARE.
 
 # Gigantum CLI Version
-__version__ = "0.13"
+__version__ = "0.14"
