aad12126c0dcbb1d61c5847fc4f6b5576833a126
Aniruddha Mysore <AniMysore74@users.noreply.github.com>
2019-03-13 20:46:32 +0530

    Catch FileNotFound when Nvidia driver not present
    Fixes Issue #42
    
    Signed-off-by: Aniruddha Mysore <aniruddha.mysore@gmail.com>

--- gigantumcli/utilities.py
+++ gigantumcli/utilities.py
@@ -68,11 +68,15 @@ def is_running_as_admin():
 def get_nvidia_driver_version() -> str:
     driver_version = None
     if platform.system() == 'Linux':
-        bash_command = "nvidia-smi --query-gpu=driver_version --format=csv,noheader"
-        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        if not error:
-            m = re.match(r"([\d.]+)", output.decode())
-            if m:
-                driver_version = m.group(0)
+        try:
+            bash_command = "nvidia-smi --query-gpu=driver_version --format=csv,noheader"
+            process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            if not error:
+                m = re.match(r"([\d.]+)", output.decode())
+                if m:
+                    driver_version = m.group(0)
+
+        except FileNotFoundError:
+            pass
     return driver_version
